ba354aa3ec08c0dad93c809075a43f7e34efea7b
Eguonor Oputu <oputueguonor@gmail.com>
2025-05-06 15:21:13 +0100

    update

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { CoinContext } from "../context/CoinContext";
+import { Link } from "react-router-dom";
 
 const Table = () => {
+  const { allCoins, currency } = useContext(CoinContext);
+  const [displayCoins, setDisplayCoins] = useState([]);
   return <div>Table</div>;
 };
 
--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -8,8 +8,39 @@
 //exporting
 import React, { createContext, useEffect, useState } from "react";
 export const CoinContext = createContext();
-const contextValue = 0;
+
 const CoinProvider = (props) => {
+  const [allCoins, setAllCoins] = useState([]);
+  const [currency, setCurrrency] = useState({ name: "ngn", symbol: "N" });
+
+  const fetchAllCoins = async () => {
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-q5ya9MNFEjHMgt8LwuFNb87v",
+      },
+    };
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .then((json) => {
+        setAllCoins(json);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchAllCoins();
+  }, [currency]);
+
+  const contextValue = {
+    allCoins,
+    currency,
+    setCurrrency,
+  };
   return (
     <CoinContext.Provider value={contextValue}>
       {props.children}
